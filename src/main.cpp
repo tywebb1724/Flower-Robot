f65cc993464909178341988d4e653e087cec3775
Ty Webb <tywebb1724@gmail.com>
2026-04-06 16:04:03 -0600

    Draft 2 (no display, servo at low level)

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 //Libraries
 #include <Arduino.h>
-#include <Servo.h>
+//#include <Servo.h>
 #include <NewPing.h>
 
 
 //Pins for sonar range finder
-#define TRIGGER_PIN A1
-#define ECHO_PIN A0
+#define TRIGGER_PIN A1 //(2nd to rightmost bit of DDRC)
+#define ECHO_PIN A0 //(Rightmost bit of DDRC)
 
 //Pins for servos
-#define SERVO_PIN 9
+//#define SERVO_PIN 9
 
 //Parameters for servos
-#define SERVO_START_ANGLE 0
+#define SERVO_START_ANGLE 90
 #define SERVO_UP_LIMIT 180
 #define SERVO_DOWN_LIMIT 0
-static Servo myServo;
+//static Servo myServo;
 
 //Sensor state definitions
 #define DETECTION_YES 1
@@ -34,7 +34,6 @@ int DetectionState = DETECTION_NO;
 int ActionServo = SERVO_CLOSE;
 static NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-
 //Function to detect if there's someone by the flower
 bool isDetected() {
   int sonar_distance = sonar.ping_cm();
@@ -46,6 +45,54 @@ bool isDetected() {
   }
 }
 
+//Convert the angle to number of ticks
+uint16_t angleToTicks(int angle) {
+  if (angle < 0) {
+    angle = 0;
+  }
+  if (angle > 180) {
+    angle = 180;
+  }
+
+  // Map 0..180 degrees to 1000..2000 us
+  uint16_t pulse_us = 1000 + ((uint32_t)angle * 1000) / 180;
+
+  // Each tick is 0.5 us, so multiply by 2
+  return pulse_us * 2;
+}
+
+//Write servo angle
+void servoWriteAngle(int angle) {
+  OCR1A = angleToTicks(angle);
+}
+
+//Set up servo
+void servoInit() {
+  //Set PB1/OC1A (Pin 9) as output
+  DDRB |= (1 << DDB1);
+
+  //Clear control registers
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  //Fast PWM mode 14; TOP = ICR1
+  TCCR1A |= (1 << WGM11);
+  TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+  // Non-inverting output on OC1A
+  TCCR1A |= (1 << COM1A1);
+
+  // Prescaler = 8
+  TCCR1B |= (1 << CS11);
+
+  // 20 ms period at 16 MHz with prescaler 8
+  // Timer tick = 0.5 us
+  // 20,000 us / 0.5 us = 40,000 ticks
+  ICR1 = 39999;
+
+  //Set initial angle
+  OCR1A = angleToTicks(SERVO_START_ANGLE);
+}
 
 void FlowerPerception() {
   if (isDetected()) {
@@ -79,19 +126,23 @@ void FlowerAction() {
     ServoAngle -= 2;
     Serial.println("Close");
   }
-  myServo.write(ServoAngle);
+  servoWriteAngle(ServoAngle);
 }
 
 
 void setup() {
   //Output pins
+  //DDRC |= (1 << DDC1); 
   pinMode(TRIGGER_PIN, OUTPUT);
   //Input pins
+  //DDRC &= ~(1 << DDC0); 
   pinMode(ECHO_PIN, INPUT);
 
   //Servo
-  myServo.attach(SERVO_PIN);
-  myServo.write(SERVO_START_ANGLE);
+  //myServo.attach(SERVO_PIN);
+  //my.Servo.write(SERVO_START_ANGLE);
+  servoInit();
+
 
   //Serial
   Serial.begin(9600);
